064ddec5b78519c91335ef92a373c250d25f78cf
luizagit <caraimanluiza@gmail.com>
2020-10-15 17:39:55 +0100

    to-top btn will take the user to the top of the page when clicked on

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,3 +23,24 @@ open.addEventListener("click", function() {
         changeIcon = true;
     }
 });
+
+// Code directed by https://www.w3schools.com
+//Get the button
+mybutton = document.getElementById("to-top-btn");
+
+// When the user scrolls down 50px from the top of the document, show the button
+window.onscroll = function() { scrollFunction(); };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the page
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
