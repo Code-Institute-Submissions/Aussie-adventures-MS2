e3ac3428bb0013d7f2c3f5122d66ccc354f6fe9c
luizagit <caraimanluiza@gmail.com>
2020-10-23 17:08:18 +0100

    uploaded photos and jquery functions filter for gallery section, corrected container width and removed global img width

--- assets/js/global-jquery.js
+++ assets/js/global-jquery.js
@@ -19,3 +19,21 @@ $(function() {
         }
     });
 });
+
+
+/*Gallery filter */
+
+$('.gallery-list-item').click(function() {
+    let value = $(this).attr('data-filter');
+    if (value === 'all') {
+        $('.filter').show(250);
+    } else {
+        $('.filter').not('.' + value).hide(250);
+        $('.filter').filter('.' + value).show(250);
+    }
+});
+
+$('.gallery-list-item').click(function() {
+    $(this).addClass('active-item').siblings().removeClass('active-item');
+
+});
